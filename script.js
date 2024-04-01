3a71357dbfcfd3e2d75750fc18bfaa65e79ab665
Juan Pablo Iacaruso <jpiacaruso@gmail.com>
2024-04-01 18:06:19 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,15 @@
+// ESTO AGREGA A LA PANTALLA EL BOTON QUE TOQUEMOS EN LA PANTALLA//
+
+function agregar(valor) {
+  document.getElementById("pantalla").value += valor;
+}
+
+function borrar() {
+  document.getElementById("pantalla").value = "";
+}
+
+function calcular() {
+  const valorPantalla = document.getElementById("pantalla").value;
+  const resultado = eval(valorPantalla);
+  document.getElementById("pantalla").value = resultado;
+}
